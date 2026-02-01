965ec59dbe45490b31f2b730eded5f287634fca3
Ashish M <ashish@ashish.org>
2026-02-01 13:59:42 -0800

    refactor: remove duplicate TempLinkItem

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export { TileCard } from './TileCard';
 export { TilePanel } from './TilePanel';
-export { TempLinkItem, type TempLink } from './TempLinkItem';
 export { DocumentEditor } from './DocumentEditor';
 export { FloatingActions } from './FloatingActions';
 export { PaletteSelector } from './PaletteSelector';
